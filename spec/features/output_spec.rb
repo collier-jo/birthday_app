bb2a30773eb59f3ebab4af3e1cd0d684a6c89d81
joannacollier <collj035@gmail.com>
2020-05-02 11:57:13 +0100

    /output webpage message changed

--- spec/features/output_spec.rb
+++ spec/features/output_spec.rb
@@ -8,20 +8,21 @@ feature 'Output page ' do
     @month = date.month
   end 
 
-
   scenario "displays Happy Birthday if date input matches current date" do
     visit("/")
+    fill_in("name", with: "Jo")
     fill_in("dd", with: "#{@day}")
     fill_in("mm", with: "#{@month}")
     click_button("Submit")
-    expect(page).to have_content("Happy Birthday")
+    expect(page).to have_content("Happy Birthday Jo")
   end
 
   scenario "displays amount of days until birthday" do
     visit("/")
+    fill_in("name", with: "Jo")
     fill_in("dd", with: "#{@day + 1}")
     fill_in("mm", with: "#{@month}")
     click_button("Submit")
-    expect(page).to have_content("1 day(s) until your birthday!")
+    expect(page).to have_content("Jo, 1 day(s) until your birthday!")
   end 
 end     
